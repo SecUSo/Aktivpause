c52be73a80d71d9d1cc7724fc5242de090da0903
coderPaddyS <urulu@student.kit.edu>
2023-12-12 08:07:37 +0100

    Adds SettingsActivity

---
+++ app/src/main/java/org/secuso/aktivpause/activities/SettingsActivity.kt
@@ -0,0 +1,28 @@
+/*
+ This file is part of the application Privacy Friendly Notes.
+ Privacy Friendly Notes is free software:
+ you can redistribute it and/or modify it under the terms of the
+ GNU General Public License as published by the Free Software Foundation,
+ either version 3 of the License, or any later version.
+ Privacy Friendly Notes is distributed in the hope
+ that it will be useful, but WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with Privacy Friendly Notes. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.secuso.aktivpause.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.secuso.aktivpause.R
+
+/**
+ * Activity that allows to register some settings like a custom font size.
+ */
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
